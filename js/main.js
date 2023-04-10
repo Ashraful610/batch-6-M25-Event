562df80d89a15d2f35968171519a4cb05cc493e6
Ashraful610 <ashalam610@gmail.com>
2023-04-11 02:23:26 +0600

    onclick

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 // console.log('hello')
+function makeTomato(){
+    document.body.style.backgroundColor = 'tomato'
+}
+
+const makeCyan = document.getElementById('makeCyan')
+makeCyan.onclick = makecyan 
+
+function makecyan(){
+    document.body.style.backgroundColor = 'skyblue'
+}
+
+const makeBlue =document.getElementById('makeBlue')
+makeBlue.onclick = function makeblue(){
+    document.body.style.backgroundColor = 'blue'
+}
