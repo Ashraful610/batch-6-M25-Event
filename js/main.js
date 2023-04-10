90bc125d92483bb0237fe6952638d7745624d720
Ashraful610 <ashalam610@gmail.com>
2023-04-11 02:42:41 +0600

    addEventlisner

--- js/main.js
+++ js/main.js
@@ -14,3 +14,13 @@ const makeBlue =document.getElementById('makeBlue')
 makeBlue.onclick = function makeblue(){
     document.body.style.backgroundColor = 'blue'
 }
+const makeRoseButton = document.getElementById('makeRose')
+makeRoseButton.addEventListener('click', makeRose)
+
+function makeRose(){
+    document.body.style.backgroundColor = 'pink'
+}
+const makeGreenButton = document.getElementById('makeGreen')
+makeGreenButton.addEventListener('click',function(){
+    document.body.style.backgroundColor = 'green'
+})
